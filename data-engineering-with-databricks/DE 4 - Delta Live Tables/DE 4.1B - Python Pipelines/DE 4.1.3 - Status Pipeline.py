2819c67002f7f020b8369a19ce29196c5766d42c
dinbab1984@gmail.com <dinbab1984@gmail.com>
2023-12-19 14:33:56 +0000

    Delta Live Tables Lab

--- data-engineering-with-databricks/DE 4 - Delta Live Tables/DE 4.1B - Python Pipelines/DE 4.1.3 - Status Pipeline.py
+++ data-engineering-with-databricks/DE 4 - Delta Live Tables/DE 4.1B - Python Pipelines/DE 4.1.3 - Status Pipeline.py
@@ -60,11 +60,11 @@
 
 # COMMAND ----------
 
-# TODO
+import dlt
 import pyspark.sql.functions as F
  
 source = spark.conf.get("source")
- 
+@dlt.table 
 def status_bronze():
     return (
         spark.readStream
@@ -80,7 +80,7 @@ def status_bronze():
  
  
 @dlt.table(
-    table_name = "status_silver"
+    name = "status_silver"
     )
 @dlt.expect_or_drop("valid_timestamp", "status_timestamp > 1640995200")
 def status_silver():
@@ -93,7 +93,7 @@ def status_silver():
 @dlt.table
 def email_updates():
     return (
-        spark.read("status_silver").alias("a")
+        dlt.read("status_silver").alias("a")
             .join(
                 dlt.read("subscribed_order_emails_v").alias("b"), 
                 on="order_id"
