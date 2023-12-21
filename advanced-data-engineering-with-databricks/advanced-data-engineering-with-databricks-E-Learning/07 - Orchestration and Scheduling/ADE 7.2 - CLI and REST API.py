36a861a5ba2c3c7af3b5736c49bc2a9a41691c7a
dinbab1984@gmail.com <dinbab1984@gmail.com>
2023-12-21 20:27:10 +0000

    CLI and API practice

--- advanced-data-engineering-with-databricks/advanced-data-engineering-with-databricks-E-Learning/07 - Orchestration and Scheduling/ADE 7.2 - CLI and REST API.py
+++ advanced-data-engineering-with-databricks/advanced-data-engineering-with-databricks-E-Learning/07 - Orchestration and Scheduling/ADE 7.2 - CLI and REST API.py
@@ -19,6 +19,68 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC # CLI examples
+# MAGIC
+# MAGIC ## Install CLI
+# MAGIC `pip3 install databricks-cli`
+# MAGIC ## Configure CLI
+# MAGIC `databricks configure --token`
+# MAGIC ## GET Clusters List
+# MAGIC `databricks clusters list`
+# MAGIC
+# MAGIC ## Get Jobs List
+# MAGIC `databricks jobs list`
+# MAGIC
+# MAGIC ## Get Jobs Runs List
+# MAGIC `databricks runs list`
+# MAGIC
+# MAGIC ## Trigger a new job (run-now)
+# MAGIC `databricks jobs run-now --job-id=763249216788681`
+# MAGIC
+# MAGIC ## Get a Job Run Info(either single task job run id or individual task run if only supported in CLI)
+# MAGIC `databricks runs get --run-id=973737584753150`
+# MAGIC
+# MAGIC ## Restart a Cluster
+# MAGIC `databricks clusters restart --cluster-id=1219-142422-5zm10ixv`
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # API examples
+# MAGIC
+# MAGIC ## GET Clusters List
+# MAGIC `curl --location --request GET 'https://adb-366368161648507.7.azuredatabricks.net/api/2.0/clusters/list' --header 'Authorization: Bearer <access_token_here>'`
+# MAGIC
+# MAGIC ## Get Jobs List
+# MAGIC `curl --location --request GET 'https://adb-366368161648507.7.azuredatabricks.net/api/2.1/jobs/list' --header 'Authorization: Bearer <access_token_here>'`
+# MAGIC
+# MAGIC ## Get Jobs Runs List
+# MAGIC `curl --location --request GET 'https://adb-366368161648507.7.azuredatabricks.net/api/2.1/jobs/runs/list?=' --header 'Authorization: Bearer <access_token_here>'`
+# MAGIC
+# MAGIC ## Trigger a new job (run-now)
+# MAGIC `curl --location --request POST 'https://adb-366368161648507.7.azuredatabricks.net/api/2.1/jobs/run-now?job_id=763249216788681' --header 'Authorization: Bearer <access_token_here>'`
+# MAGIC
+# MAGIC ## Get a Job Run Info
+# MAGIC `curl --location --request GET 'https://adb-366368161648507.7.azuredatabricks.net/api/2.1/jobs/runs/get?run_id=29894235171072' --header 'Authorization: Bearer <access_token_here>'`
+# MAGIC
+# MAGIC ## Restart a Cluster
+# MAGIC `curl --location --request POST 'https://adb-366368161648507.7.azuredatabricks.net/api/2.0/clusters/restart?cluster_id=1219-142422-5zm10ixv' --header 'Authorization: Bearer <access_token_here>'`
+# MAGIC
+# MAGIC ## Get Cluster Info
+# MAGIC `curl --location --request GET 'https://adb-366368161648507.7.azuredatabricks.net/api/2.0/clusters/get?cluster_id=1219-142422-5zm10ixv' --header 'Authorization: Bearer <access_token_here>'`
+
+# COMMAND ----------
+
+# MAGIC %sh sudo apt-get install jq
+
+# COMMAND ----------
+
+# MAGIC %sh curl --location --request GET 'https://adb-366368161648507.7.azuredatabricks.net/api/2.0/clusters/list' --header 'Authorization: Bearer <access-token-here>' | jq .
+
+# COMMAND ----------
+
 # MAGIC %md-sandbox
 # MAGIC &copy; 2022 Databricks, Inc. All rights reserved.<br/>
 # MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
